4a7d61934c7980e2a0919963d6d7578cb0fe1935
ABBURIVENKATASIVARAMAKRISHNA <avsrk2002@gmial.com>
2023-12-14 13:50:55 +0530

    removed password attribute from User

--- src/com/jsp/User.java
+++ src/com/jsp/User.java
@@ -5,7 +5,6 @@ public class User {
 	private int userId;
 	private String userName;
 	private String userEmail;
-	private String userPassword;
 	public int getUserId() {
 		return userId;
 	}
@@ -23,12 +22,6 @@ public class User {
 	}
 	public void setUserEmail(String userEmail) {
 		this.userEmail = userEmail;
-	}
-	public String getUserPassword() {
-		return userPassword;
-	}
-	public void setUserPassword(String userPassword) {
-		this.userPassword = userPassword;
 	}
 	
 }
